037f96c0c8f122675a95868545f412972bf9f005
Nicodl05 <78737983+Nicodl05@users.noreply.github.com>
2022-11-17 09:46:17 +0100

    feat: test intelij

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        // display list of number from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
         System.out.println("Hello my name is nicolas!");
     }
 }
